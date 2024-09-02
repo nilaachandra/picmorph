bceabd6d8365823caa7a7679b64918bec8c9594a
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-09-02 21:13:48 +0530

    added og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,25 @@
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"], weight: ["400", "800"] });
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "800"] });
 export const metadata: Metadata = {
   title: "PicMorph",
-  description: "Privacy Guaranteed: Image Conversion Without Servers, on your Browser only!",
+  description: "Privacy Guaranteed! Image Conversion & Compression Without Servers, everything on your Browser!",
+  icons: {
+    icon: ["picmorph-logo.webp"],
+    shortcut: ["picmorph-logo.webp"],
+  },
+  openGraph: {
+    title: "PicMorph",
+    description: "Privacy Guaranteed: Image Conversion Without Servers, on your Browser only!",
+    url: "https://picmorph.nilaacodes.me",
+    siteName: "PicMorph",
+    images: [{ url: "/picmorph-og.webp", width: 1200, height: 630 }],
+    locale: "en-IN",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -17,9 +30,8 @@ export default function RootLayout({
   return (
     <html className={poppins.className} lang="en" suppressHydrationWarning>
       <body className="bg-zinc-900 text-white">
-
         {children}
-
+        <Analytics />
       </body>
 
     </html>
